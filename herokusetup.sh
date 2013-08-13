588b6001a0bfc54171667f4eea9e4a0dcae89a1b
Rickard <rickard.edholm@gmail.com>
2013-08-13 15:49:58 +0200

    clean up setuall and add new files

---
+++ herokusetup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+i
+# heroku login
+# ssh-keygen -t rsa (enter, enter)
+# heroku keys:add
+
+# heroku create
+# git push heroku master
+
+
